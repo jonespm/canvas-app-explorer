c36a5e97686e16f5d3f4a21d38897765a9e0c402
Code Hugger (Matthew Jones) <jonespm@umich.edu>
2025-11-24 17:10:23 -0500

    Changing to check Django setting

--- backend/middleware/force_https.py
+++ backend/middleware/force_https.py
@@ -1,11 +1,12 @@
 # middleware.py
+from django.conf import settings
+
 class ForceHttpsMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        # Monkey-patch the request method
-        request.is_secure_orig = request.is_secure
-        request.is_secure = lambda: True
-        response = self.get_response(request)
-        return response
+        if getattr(settings, "SESSION_COOKIE_SECURE", False):
+            request.is_secure_orig = request.is_secure
+            request.is_secure = lambda: True
+        return self.get_response(request)
